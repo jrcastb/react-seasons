42642a4a2bd755d77c4380e5c81f78fc51dd8e66
jrcastb <jrcastillo96@hotmail.com>
2019-03-26 02:52:59 -0500

    modified

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),//success callback se muestra cada vez que la funcion determina la posición del usuario
-        (err) => console.log(err)//failure callback ya que se muestra cada vez que la funcion no logre determinar la posición
-    );
-    return <div>Latitud: </div>
 
-};
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),//success callback se muestra cada vez que la funcion determina la posición del usuario
+            (err) => console.log(err)//failure callback ya que se muestra cada vez que la funcion no logre determinar la posición
+        );
 
+        return <div>Latitud: </div>;
+    }
+}
 ReactDOM.render(
     <App />,
     document.querySelector("#root")
